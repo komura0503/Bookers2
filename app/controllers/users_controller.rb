9f1ac094057ea9407d6050ca9e97c61bdf8724fa
supernemutai <tokikake66@gmail.com>
2021-09-23 17:26:32 +0000

    [Finish]4-10

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+    @book=Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render:edit
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @book=Book.new
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render:edit
+    end
+  end
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '17c63f35b7a4c44205ad01cdc3475e52527d43a587b4d0d0c465dbdde5a7dd3b24d17cc716591c462040b442a88fcf48c167d68d3a66df9b0350b6eb9c518c89'
